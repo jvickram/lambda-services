704a965680d085f90f2282676088fe190d742588
Bickey Jaisawal <jvickram2@gmail.com>
2020-05-30 16:09:55 +0530

    Boiler plate for lambda

---
+++ handler.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.firstFunction = async event => {
+  console.log('Event is ', event)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Yeah! dude, you got it',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
